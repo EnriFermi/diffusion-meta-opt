57bedf52c04a01f701b90447d6df517e2a9f6907
EnriFermi <metaphysical@hello.io>
2026-02-13 19:54:04 +0100

    add train + VAE

---
+++ models/__init__.py
@@ -0,0 +1,8 @@
+from .weight_quantile_vae import EncoderConfig, ModelConfig, ResamplerConfig, WeightQuantileVAE
+
+__all__ = [
+    "EncoderConfig",
+    "ModelConfig",
+    "ResamplerConfig",
+    "WeightQuantileVAE",
+]
