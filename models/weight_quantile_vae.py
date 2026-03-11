ab953d4c166be081f15b81f0d1a632f6c0bd6cae
EnriFermi <metaphysical@hello.io>
2026-03-11 19:32:36 +0100

    make batch identical

--- models/weight_quantile_vae.py
+++ models/weight_quantile_vae.py
@@ -2460,7 +2460,9 @@ class BigWeightVAE(nn.Module):
         if squeeze_batch:
             W = W.unsqueeze(0)
             X = X.unsqueeze(0)
-        W = W[0].repeat(W.shape[0], 1, 1)
+        if W.shape[0] > 1:
+            W = W[0:1].repeat(W.shape[0], 1, 1)
+            X = X[0:1].repeat(X.shape[0], 1, 1)
         B, d_in, d_out = W.shape
         Bx, n, d_in_x = X.shape
         if Bx != B or d_in_x != d_in:
