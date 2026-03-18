54795cf8d5081314afc61d39af6ff9f8718501e7
EnriFermi <metaphysical@hello.io>
2026-03-18 01:39:24 +0100

    add new sandbox

---
+++ procedural_simple_vae_pipeline/__init__.py
@@ -0,0 +1,6 @@
+from procedural_simple_vae_pipeline.model import ProceduralSimpleBigWeightVAE, build_procedural_simple_vae
+
+__all__ = [
+    "ProceduralSimpleBigWeightVAE",
+    "build_procedural_simple_vae",
+]
---
+++ procedural_simple_vae_pipeline/run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+if [ -n "${CONDA_PREFIX:-}" ]; then
+  export LD_LIBRARY_PATH="$CONDA_PREFIX/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+fi
+
+if [ -n "${PIPENV_ACTIVE:-}" ]; then
+  exec python -m procedural_simple_vae_pipeline.train "$@"
+fi
+
+if [ -n "${CONDA_PREFIX:-}" ]; then
+  exec python -m procedural_simple_vae_pipeline.train "$@"
+fi
+
+if command -v pipenv >/dev/null 2>&1; then
+  exec pipenv run python -m procedural_simple_vae_pipeline.train "$@"
+fi
+
+exec python -m procedural_simple_vae_pipeline.train "$@"
