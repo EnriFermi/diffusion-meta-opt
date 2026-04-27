0f81711946ad91278beee8b50dd72c6e811c7a91
EnriFermi <metaphysical@hello.io>
2026-04-27 20:00:40 +0200

    fix

--- post_train_research/vit_latent_scaling/run_vit_latent_scaling_ae_diffusion_prior.sh
+++ post_train_research/vit_latent_scaling/run_vit_latent_scaling_ae_diffusion_prior.sh
@@ -13,7 +13,7 @@ BIG_VAE_DIFFUSION_PRIOR_CHECKPOINT=""
 BIG_VAE_INIT_CALIBRATION_BATCHES="1"
 LATENT_LR_SCHEDULER="cosine_decay_to_floor"
 LATENT_LR_FLOOR_RATIO="0.1"
-LATENT_LR_DECAY_FRACTION="0.7"
+LATENT_LR_DECAY_STEPS="5250"
 
 : "${BIG_VAE_CHECKPOINT:?Edit BIG_VAE_CHECKPOINT in this script before running}"
 : "${BIG_VAE_DIFFUSION_PRIOR_CHECKPOINT:?Edit BIG_VAE_DIFFUSION_PRIOR_CHECKPOINT in this script before running}"
@@ -43,4 +43,4 @@ run_python \
   "vit_latent_scaling.big_vae_init_calibration_batches=$BIG_VAE_INIT_CALIBRATION_BATCHES" \
   "vit_latent_scaling.latent_lr_scheduler=$LATENT_LR_SCHEDULER" \
   "vit_latent_scaling.latent_lr_floor_ratio=$LATENT_LR_FLOOR_RATIO" \
-  "vit_latent_scaling.latent_lr_decay_fraction=$LATENT_LR_DECAY_FRACTION"
+  "vit_latent_scaling.latent_lr_decay_steps=$LATENT_LR_DECAY_STEPS"
